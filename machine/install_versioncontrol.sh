500c9ef7f9bf23c7030f51029165883b7a4a0767
james.duncan <jimmyduncan@live.com.au>
2017-06-03 14:21:48 +1000

    vagrant conf for shared folder, git provision

--- machine/install_versioncontrol.sh
+++ machine/install_versioncontrol.sh
@@ -2,4 +2,6 @@
 echo ============================================
 echo =         Installing Version Control       =
 echo ============================================
-sudo yum install git -assumeyes
+sudo yum install git --assumeyes
+mv /srv/share_specific/key_* ~/.ssh/
+chmod 400 ~/.ssh/key_*
